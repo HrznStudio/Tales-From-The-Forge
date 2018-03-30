e6d7c8885e0330f3d199d194c14bcab10a11c7ce
Lanse <simon_stalnabb@hotmail.com>
2018-03-30 19:08:11 +0200

    Added a Constants Class and fixed up the main class

---
+++ src/main/java/projecthrzn/talesfromtheforge/TalesFromTheForge.java
@@ -0,0 +1,24 @@
+package projecthrzn.talesfromtheforge;
+
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+import projecthrzn.talesfromtheforge.util.TFTFConstants;
+
+@Mod(modid = TFTFConstants.MODID, name = TFTFConstants.NAME, version = TFTFConstants.VERSION, dependencies = TFTFConstants.DEPS, acceptedMinecraftVersions = TFTFConstants.MCVERS)
+public class TalesFromTheForge {
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event) {
+        
+    }
+}
---
+++ src/main/java/projecthrzn/talesfromtheforge/util/TFTFConstants.java
@@ -0,0 +1,15 @@
+package projecthrzn.talesfromtheforge.util;
+
+import java.util.Random;
+
+public class TFTFConstants {
+    public static final String MODID = "talesfromtheforge";
+    public static final String NAME = "Tales From The Forge";
+    public static final String VERSION = "1.12.2-1.0.0";
+    public static final String DEPS = "required-after:forge@[14.23.1.2640,);after:mcmultipart;after:ctm";
+    public static final String MCVERS = "[1.12,1.12.2]";
+
+    // System Constants
+    public static final Random RANDOM = new Random();
+
+}
